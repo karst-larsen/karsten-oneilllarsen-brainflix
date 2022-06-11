991d61087d7094c389345557f82d9367d23b87f3
Karsten Larsen <karsten.oneill.larsen@mail.utoronto.ca>
2022-06-11 14:35:09 -0400

    refactored upload page functionality to consolidate submit event into one function rather than two for upload header notification

--- src/pages/VideoSection/VideoSection.js
+++ src/pages/VideoSection/VideoSection.js
@@ -9,59 +9,48 @@ class VideoSection extends Component {
     //Initialize state prior to component mount
     state = {
         currentVideo: null,
-        nextVideos: []
+        nextVideos: [],
+        uploadSuccess: false
       };
 
     componentDidMount() {
     //Retrieve API for video data
-    const API_URL = `https://project-2-api.herokuapp.com/videos/?api_key=`;
-    const API_KEY = "01e77bc7-e260-474d-a7f8-d01e8c44f12e";
     axios.get('http://localhost:8080/videos')
     .then(response => {
         this.setState( {
-            nextVideos: response.data
+            nextVideos: response.data,
+            uploadSuccess: this.props.match.params.complete
             });
             const currentVideoId = response.data[0].id
             this.fetchMainVideo(currentVideoId)
         })
     }
-    // axios.get(`${API_URL}${API_KEY}`).then((response) => {
-    //     this.setState({
-    //         nextVideos: response.data,
-    //     });
-    //     const currentVideoId = response.data[0].id
-    //     this.fetchMainVideo(currentVideoId)
-    //     })
-    // }
     
     componentDidUpdate(prevProps) {
     //Update the video information based on which video or link (BrainFlix logo) the user has clicked on
         window.scrollTo(0, 0)
         const previousVideoId = prevProps.match.params.id;
         const currentVideoId = this.props.match.params.id;
+
+        //Remove the upload notification if the user clicks the BrainFlix Logo, goes to the upload page, or selects a new video
+        if (this.props.match.params.complete !== prevProps.match.params.complete) {
+                this.setState({
+                    uploadSuccess: false
+                })
+        }
         
         if (previousVideoId !== currentVideoId) {
             //Clicking the BrainFlix logo when the user has another video selected causes an undefined ID
             //This if condition covers this case by running the componentDidMount initial API retrieval
             if (currentVideoId === undefined) {
-                // const API_URL = `https://project-2-api.herokuapp.com/videos/?api_key=`;
-                // const API_KEY = "01e77bc7-e260-474d-a7f8-d01e8c44f12e";
-                // axios.get(`${API_URL}${API_KEY}`).then((response) => {
-                //     this.setState({
-                //         nextVideos: response.data,
-                //     });
-                //     const currentVideoId = response.data[0].id
-                //     this.fetchMainVideo(currentVideoId)
-                // })
                 axios.get('http://localhost:8080/videos')
                 .then(response => {
                     this.setState( {
-                        nextVideos: response.data
+                        nextVideos: response.data,
                         });
                         const currentVideoId = response.data[0].id
                         this.fetchMainVideo(currentVideoId)
                     })
-                
                 return
             }
             //If the currentVideoId is not undefined, update the currentVideo to the ID of the video that the user clicked on
@@ -71,40 +60,43 @@ class VideoSection extends Component {
 
     fetchMainVideo = (videoId) => {
         //Fetch the currentVideo to the ID of the video that the user clicked on
-        const API_URL = `https://project-2-api.herokuapp.com/videos/${videoId}?api_key=`;
-        const API_KEY = "01e77bc7-e260-474d-a7f8-d01e8c44f12e";
-        axios.get(`${API_URL}${API_KEY}`)
-        .then(response => {
-            this.setState({
-                currentVideo: response.data
-            }, () => document.title = `BrainFlix - ${response.data.title}`)
-        })
+        axios.get(`http://localhost:8080/videos/${videoId}`)
+                .then(response => {
+                    this.setState( {
+                        currentVideo: response.data
+                        }, () => document.title = `BrainFlix - ${response.data.title}`);
+                    })
     }
 
     //Passing down retrieved state of the current main video and next videos array to children components
     render() {
-        const { currentVideo, nextVideos } = this.state;
+        const { currentVideo, nextVideos, uploadSuccess } = this.state;
         return (
             <>
+            <div className={`header__upload-notification ${uploadSuccess === 'uploadComplete' ? 'header__upload-notification--success' : ''}`}><span>Upload Complete!</span></div>
                 <section className="video-section">
                     {!currentVideo && <span>Loading...</span>}
+                    {currentVideo && 
+                    <>
                     <div className="video">
-                    {currentVideo && <MainVideo mainVideo={currentVideo}/>}
+                        <MainVideo mainVideo={currentVideo}/>
                     </div>
-                    <div className="desktop__wrapper">
-                        <section className="desktop__section-left">
-                            {currentVideo && <VideoDetails mainVideo={currentVideo} />}
-                            <div className="comments">
-                            {currentVideo && <Comments mainVideo={currentVideo}/>}
-                            </div>
-                        </section>
-                        <section className="next-section">
-                            {currentVideo && <NextVideos 
-                            mainVideo={currentVideo}
-                            nextVideos={nextVideos}
-                            />}
-                        </section>
-                    </div>
+                        <div className="desktop__wrapper">
+                            <section className="desktop__section-left">
+                                <VideoDetails mainVideo={currentVideo} />
+                                <div className="comments">
+                                <Comments mainVideo={currentVideo} />
+                                </div>
+                            </section>
+                            <section className="next-section">
+                                <NextVideos 
+                                mainVideo={currentVideo}
+                                nextVideos={nextVideos}
+                                />
+                            </section>
+                        </div>
+                    </>
+                }
                 </section>
             </>
         )
